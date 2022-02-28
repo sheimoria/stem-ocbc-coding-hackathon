e68747d5737aab8cdc73785bf9809d590e3d02b9
Shem Maleriado <shem.maleriado@gmail.com>
2022-02-28 10:54:19 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# personal-website
+
+- **Framework**: [Next.js](https://nextjs.org/)
+- **Styling**: [Tailwind CSS](https://tailwindcss.com/)
+- **Deployment**: [Vercel](https://vercel.com)
+
+## Installation
+
+1. Run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+2. Open [http://localhost:3000](http://localhost:3000) with your browser.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography')
+  ]
+}
